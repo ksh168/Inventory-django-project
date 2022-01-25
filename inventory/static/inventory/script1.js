8867e84fa163c47a71353f91e92c29c556d7524f
Ubuntu <azureuser@azure-test2.uq3as44zh0sefefhhwwcfy1z1d.bx.internal.cloudapp.net>
2022-01-25 19:32:29 +0000

    added popup modal

--- inventory/static/inventory/script1.js
+++ inventory/static/inventory/script1.js
@@ -1,9 +1,9 @@
-document.getElementById('add-new-btn').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "flex";
+document.getElementById("add-new-btn").addEventListener("click", function() {
+	document.querySelector(".bg-modal").style.display = "flex";
 	console.log("add-new-btn clicked");
 });
 
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "none";\
+document.querySelector(".close").addEventListener("click", function() {
+	document.querySelector(".bg-modal").style.display = "none";
 	console.log("close clicked");
 });
