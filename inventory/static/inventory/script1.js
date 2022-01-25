e9d3aa82168d9766d51be65562f108dfac2094ea
Ubuntu <azureuser@test1-MB.uq3as44zh0sefefhhwwcfy1z1d.bx.internal.cloudapp.net>
2022-01-25 18:52:57 +0000

    trying to add pop-up

---
+++ inventory/static/inventory/script1.js
@@ -0,0 +1,9 @@
+document.getElementById('add-new-btn').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+	console.log("add-new-btn clicked");
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";\
+	console.log("close clicked");
+});
--- inventory/views.py
+++ inventory/views.py
@@ -5,20 +5,20 @@ from .models import Product
 
 
 #dummy data
-items = [
-    {
-        'product_name': 'Sanitizier',
-        'price': 50,
-        'quantity': 2,
-        'date_posted': 'Jan 25 2022'
-    },
-    {
-        'product_name': 'Cup',
-        'price': 30,
-        'quantity': 5,
-        'date_posted': 'Jan 25 2022'
-    }
-]
+# items = [
+#     {
+#         'product_name': 'Sanitizier',
+#         'price': 50,
+#         'quantity': 2,
+#         'date_posted': 'Jan 25 2022'
+#     },
+#     {
+#         'product_name': 'Cup',
+#         'price': 30,
+#         'quantity': 5,
+#         'date_posted': 'Jan 25 2022'
+#     }
+# ]
 
 
 def home(request):
